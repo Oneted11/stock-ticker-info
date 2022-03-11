302f3592b5bcb19ae85e6257e027b6a3c0c6da16
Oneted11 <muriithited@gmail.com>
2022-03-11 08:46:45 +0300

    add readme

---
+++ readme.md
@@ -0,0 +1,13 @@
+# How to run
+
+## Development
+
+```bash
+export FLASK_ENV=development && flask run
+```
+
+## Production
+
+```bash
+flask run
+```
--- run.py
+++ run.py
@@ -1,50 +1,38 @@
-
 from flask import Flask, request, render_template, url_for, flash, redirect, request, session, jsonify
-from flask_bootstrap import Bootstrap
-import finnhub
-from flask_moment import Moment
-import json
-import datetime
-from dateutil.relativedelta import relativedelta, MO
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'SECRETKEYSECRETKEY'
-
-moment = Moment(app)
-
-finnhub_client = finnhub.Client(api_key="c8kdqgqad3ibbdm3p040")
 
 
-@app.route('/', methods=["GET", "POST"])
-def gfg():
-    # if request.method == "POST":
-    #     ticker_value = "TSLA"
-    #     profile = finnhub_client.company_profile2(symbol=ticker_value)
-    #     quotes = finnhub_client.quote(ticker_value)
-    #     recommend = finnhub_client.recommendation_trends(ticker_value)
-    #     recommend = json.dumps(recommend)
-    #     candles = finnhub_client.stock_candles(
-    #         (ticker_value), 'D', 1590988249, 1591852249)
-
-    #     today = datetime.datetime.now()
-    #     today.strftime("%Y-%m-%d")
-    #     days = datetime.timedelta(days=30)
-    #     days_prior = today - days
-    #     days_prior.strftime("%Y-%m-%d")
-    #     news = finnhub_client.company_news(
-    #         ticker_value, _from=days_prior, to=today)
-    #     news = json.dumps(news)
-
-    #     merged_data = profile.copy()
-    #     merged_data.update(quotes)
-    #     merged_data.update(candles)
-
-    #     return merged_data
-
-    return render_template("index.html")
+@app.route("/")
+def default():
+    return "<h1>HELLO WORLD!!!</h1>"
 
 
-bootstrap = Bootstrap(app)
+@app.route('/print')
+def printMsg():
+    app.logger.warning("testing testing warning 123")
+    app.logger.error("thsi is an error log from the user")
+    app.logger.info("this isjust testing the info logging")
+    return "Check the bloody console, where its running"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
